d26a06d604c5086eb218f89d53c86a6deda4dae4
raul <perlaxd365@gmail.com>
2026-03-13 09:12:49 -0500

    primer avance

--- guardar.php
+++ guardar.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 $conn = new mysqli("localhost", "cesarxd365", "&DGFl8=n(_qU,", "clinicabahia");
 
 if ($conn->connect_error) {
