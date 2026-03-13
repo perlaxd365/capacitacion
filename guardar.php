174f1aa387a6328d3389ea0cbe3d91f90d91e96a
raul <perlaxd365@gmail.com>
2026-03-13 09:00:52 -0500

    primer avance

---
+++ guardar.php
@@ -0,0 +1,32 @@
+<?php
+
+$servername = "132.148.181.41";
+$username = "cesarxd365";
+$password = "&DGFl8=n(_qU,";
+$dbname = "clinicabahia";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$dni = $_POST['dni'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$curso = $_POST['curso'];
+
+$sql = "INSERT INTO matriculas 
+(nombre,apellido,dni,telefono,correo,curso)
+VALUES 
+('$nombre','$apellido','$dni','$telefono','$correo','$curso')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro guardado correctamente";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
