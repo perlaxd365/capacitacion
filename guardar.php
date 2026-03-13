d5a1214144049f9a223032ed6aa1945c22f47fba
raul <perlaxd365@gmail.com>
2026-03-13 09:18:26 -0500

    primer avance

--- guardar.php
+++ guardar.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli("localhost", "cesarxd365", "TU_PASSWORD", "clinicabahia");
+$conn = new mysqli("localhost", "cesarxd365", "~-aIye)Wol]b", "clinicabahia");
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
