ff5a38993fe2beabea70d06eddf58a17751c5473
raul <perlaxd365@gmail.com>
2026-03-13 09:17:19 -0500

    primer avance

--- guardar.php
+++ guardar.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli("localhost", "cesarxd365", "~-aIye)Wol]b", "clinicabahia");
+$conn = new mysqli("localhost", "cesarxd365", "TU_PASSWORD", "clinicabahia");
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
@@ -22,8 +22,10 @@ VALUES
 ('$nombre','$apellido','$dni','$telefono','$correo','$curso')";
 
 if ($conn->query($sql)) {
-    echo "Registro guardado correctamente";
+
+    header("Location: index.html?registro=ok");
 } else {
+
     echo "Error: " . $conn->error;
 }
 
